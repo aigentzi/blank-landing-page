828bcf27d3282e2406fb49910370e1e6fb5e6341
Sam Durrant <sam@tixae.ai>
2025-11-12 16:38:08 +1300

    feat: add Next.js redirect to studio.xtrius.com

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["tixae.ai"],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://studio.xtrius.com/:path*',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
